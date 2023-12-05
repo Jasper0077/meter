0b548742103b67b55aed0936dcdb5c55ff0e35e9
Jasper0077 <jasperhwong@hotmail.com>
2023-12-05 18:41:33 +0800

    fix: set data point to 1

--- data.ts
+++ data.ts
@@ -1,113 +1,108 @@
 import { Question } from "./types";
 
 let id = 1;
-const questions: Array<Question> = [
+export const questions: Array<Question> = [
   {
     id: id++,
     question: "我相信上帝为我安排最好的计划，并相信祂有完美的时机。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我把我的忧虑交给上帝，相信祂会照顾我的一切。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我寻求在试炼和挣扎中辨别出上帝给我的教训。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "面对危机、问题或意想不到的情况时，我会紧紧依靠神。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question:
       "我相信即使在我对事物不确定，当我不明白为什么会发生不如意之事的时候，一切仍然是在上帝掌控之中。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我每天花时间研读上帝的话语。",
-    point: 0,
-  },
-  {
-    id: id++,
-    question: "我每天花时间研读上帝的话语。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "当我的行为违背神的诫命时，我会认罪并悔改。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "即使当我心不愿意，我还是会选择顺服上帝的话语.",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我根据上帝的话语做人生的决定。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我顺服上帝的话语是出于对祂的爱有所感恩，而不是因着愧疚或恐惧。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我积极参与小组团契。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我竭力与在基督里的弟兄姊妹们建立和加深属灵的友情。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我知道并会尽我所能帮助主内弟兄姐妹们的需要。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "在我与基督同行及成长的过程中，我有一位敬虔的导师扶助我。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我为教会和其他人祈祷。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我花时间在静默和独处中，以辨别上帝的旨意。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我每天都腾出时间祷告，与神沟通。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question:
       "我的财务预算，首先是奉献给上帝，这乃出于喜乐和敬拜的心，而不是出于义务。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question: "我每周都会和其他信徒一起参加主日崇拜（在线/现场）。",
-    point: 0,
+    point: 1,
   },
   {
     id: id++,
     question:
       "我每天都抽出时间进行反思——感谢上帝的祝福并为任何错误的行为忏悔。",
-    point: 0,
+    point: 1,
   },
 ];
